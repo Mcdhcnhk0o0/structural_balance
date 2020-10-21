0d0b6dae6c2629d42ac137573a387ca508c7ecf9
WQQDuan <726395234@qq.com>
2020-10-21 22:30:32 +0800

    调整module的导入方式，简化import

---
+++ module/__init__.py
@@ -0,0 +1,5 @@
+from .frustration import Frustration
+from .local_search import LocalSearch
+from .neighborhood import Neighborhood
+from .initialization import Initialization
+from .objective_function import ObjectiveFunction
